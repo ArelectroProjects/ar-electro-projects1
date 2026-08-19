a20291b6951a78e3ff6267dad1256da8738a1ddd
ArelectroProjects <87817833+ArelectroProjects@users.noreply.github.com>
2026-08-19 20:41:44 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -477,7 +477,11 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=[
+        "https://melodic-empanada-add294.netlify.app",
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
